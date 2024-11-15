ccb2b064c47e2d5de4f612d5131f53d60d851114
Admin <Admin@PapunPC>
2024-11-15 13:44:22 +0530

    third message  pull

---
+++ Automationframework/src/test/java/github/Github1.java
@@ -0,0 +1,12 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Github1 {
+	@Test
+	public void addoroneMessage() {
+		Reporter.log("Welcome to my GitHub profile",true);
+	}
+
+}
