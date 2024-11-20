2b40abd50ed258029be41b01554c849705a1857d
Admin <Admin@PapunPC>
2024-11-20 14:00:05 +0530

    onPullscm creted successful added

---
+++ Automationframework/src/test/java/onPullSCM/onPullscm.java
@@ -0,0 +1,12 @@
+package onPullSCM;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class onPullscm {
+	@Test
+	public void addonPullSCM() {
+		Reporter.log("onPullscm creted successful",true);
+	}
+
+}
